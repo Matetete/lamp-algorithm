0b2b9214d42a210a4b34765d29397c51616e70d2
Matetete <86987488+Matetete@users.noreply.github.com>
2021-07-10 00:55:04 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,17 +1,3 @@
-// let numero = prompt("Coloque un número")
-
-// if (numero >= 0 && numero <= 9) {
-// 	alert("Su número esta entre 0 y 9")
-// }
-
-// else if (numero >= 10 && numero <= 20) {
-// 	alert("Su número esta entre 10 y 20")
-// }
-
-// else {
-// 	alert("Su número no aparece en mi sistema")
-// }
-
 
 
 // let grados = prompt("¿Cuántos grados hace?")
